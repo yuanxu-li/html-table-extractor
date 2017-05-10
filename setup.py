7baebf8cec40ba58d071884a21f421fd62c3ef02
Yuanxu Li <yuanxu.lee@gmail.com>
2017-05-09 23:22:43 -0400

    test for python3

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open(os.path.join(here, 'README.md')) as f:
 setup(
     name='html-table-extractor',
     url='https://github.com/yuanxu-li/html-table-extractor',
-    version='1.2.0',
+    version='1.3.0',
     author='Justin Li',
     author_email='yuanxu.lee@gmail.com',
     description='A python library for extracting data from html table',
@@ -29,6 +29,8 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
     ],
     keywords='html table beautifulsoup crawler scrape',
     packages=[
